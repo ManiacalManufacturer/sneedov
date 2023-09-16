79a0580615e077edf87d829b436efe26217adc49
Sneed <Sneed@Linux-PC>
2023-09-16 20:22:10 +0300

    Added markov generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
+use rand::prelude::*;
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::io::Error;
 
 const START_KEYWORD: &str = "__start";
 const END_KEYWORD: &str = "__end";
+const PUNCTUATION: [char; 6] = ['.', ',', '?', '!', ';', ':'];
 
 fn split_punctuation(split: Vec<&str>) -> Vec<String> {
-    let punctuation = ['.', ',', '?', '!', ';', ':'];
-
     let list = split.iter();
     let mut vec = Vec::<String>::new();
     for value in list {
@@ -21,7 +20,7 @@ fn split_punctuation(split: Vec<&str>) -> Vec<String> {
                 match second_last_char {
                     Some(..) => {
                         if last_char.unwrap() != second_last_char.unwrap()
-                            && punctuation.contains(&last_char.unwrap())
+                            && PUNCTUATION.contains(&last_char.unwrap())
                         {
                             vec.push(String::from(word.as_str()));
                             vec.push(String::from(last_char.unwrap()));
@@ -62,7 +61,7 @@ fn return_index(file: &File, word: &str) -> Option<usize> {
     None
 }
 
-fn write_to_file(file: &mut File, string: &str) -> Result<(), Error> {
+fn write_to_file(file: &mut File, string: &str) -> Result<(), std::io::Error> {
     if let Err(e) = writeln!(file, "{}", string) {
         eprintln!("Error: Could not write to file: {}", e);
         return Err(e);
@@ -98,7 +97,64 @@ fn increment_line(line: &str, index: usize) -> String {
     new_line
 }
 
-fn add_word(filename: &str, keyword: &str) -> Result<usize, Error> {
+fn get_occurances(line: &str) -> Result<Vec<(usize, usize)>, Box<dyn std::error::Error>> {
+    let mut split_line = line.split_whitespace();
+    let mut vec: Vec<(usize, usize)> = vec![];
+
+    split_line.next();
+    for word in split_line {
+        let mut split = word.split(":");
+        let tuple = (
+            split.next().unwrap().parse::<usize>().unwrap(),
+            split.next().unwrap().parse::<usize>().unwrap(),
+        );
+        vec.push(tuple)
+    }
+    Ok(vec)
+}
+
+fn get_line(filename: &str, index: usize) -> Result<String, std::io::Error> {
+    let file = OpenOptions::new()
+        .read(true)
+        .open(format!("{}words", filename))
+        .unwrap();
+
+    let mut reader = BufReader::new(&file);
+    let mut string = String::new();
+    let _ = reader.read_to_string(&mut string);
+
+    let line = string.split("\n").nth(index).unwrap().to_owned();
+
+    Ok(line)
+}
+
+fn get_word(lineresult: &Result<String, std::io::Error>) -> String {
+    let line = lineresult.as_ref();
+    let mut split_line = line.unwrap().split_whitespace();
+    split_line.next().unwrap().to_owned()
+}
+
+fn get_next_word(vec: Vec<(usize, usize)>) -> usize {
+    //let mut total_vec: Vec<usize> = vec![];
+    //let tuple_vec: Vec<(usize, usize)> = vec.clone();
+
+    // for instance in vec {
+    //     total_vec.push(instance.1);
+    // }
+
+    // let sum: usize = total_vec.iter().sum();
+    // let mut adjusted_vec: Vec<(usize, usize)> = tuple_vec.clone();
+
+    // for instance in tuple_vec {
+    //     let divided = instance.1 / sum;
+    //     adjusted_vec.push((instance.0, divided));
+    // }
+
+    let mut rng = thread_rng();
+    vec.choose_weighted(&mut rng, |item| item.1).unwrap().0
+}
+
+fn add_word(filename: &str, keyword: &str) -> Result<usize, std::io::Error> {
     let mut file = OpenOptions::new()
         .read(true)
         .append(true)
@@ -123,7 +179,33 @@ fn add_word(filename: &str, keyword: &str) -> Result<usize, Error> {
     Ok(count as usize)
 }
 
-pub fn set_keywords(filename: &str) -> Result<(), Error> {
+fn add_sanitized_word(filename: &str, keyword: &str) -> Result<usize, std::io::Error> {
+    if keyword == START_KEYWORD || keyword == END_KEYWORD {
+        let mut new_word = String::new();
+        new_word.push('_');
+        new_word.push_str(keyword);
+        return add_sanitized_word(filename, new_word.as_str());
+    }
+    add_word(filename, keyword)
+}
+
+fn fix_fake_keyword(keyword: &str) -> &str {
+    if keyword == "___start" || keyword == "___end" {
+        let mut chars = keyword.chars();
+        chars.next();
+        return chars.as_str();
+    }
+    keyword
+}
+
+fn is_punctuation(charresult: Result<char, std::char::ParseCharError>) -> bool {
+    match charresult {
+        Ok(c) => PUNCTUATION.contains(&c),
+        Err(_) => false,
+    }
+}
+
+pub fn set_keywords(filename: &str) -> Result<(), std::io::Error> {
     if let Err(e1) = add_word(filename, END_KEYWORD) {
         return Err(e1);
     }
@@ -139,14 +221,18 @@ pub fn set_keywords(filename: &str) -> Result<(), Error> {
 ///
 ///* `filename` -  File name for the file to save to
 ///* `word` -  The word that gets appended
-///* `filename` -  The future word that occurs after the current one
+///* `next_word` -  The future word that occurs after the current one
 ///
-pub fn sneedov_append(filename: &str, word: &str, next_word: &str) -> Result<(), Error> {
-    let result = add_word(filename, word);
+pub fn sneedov_append_word(
+    filename: &str,
+    word: &str,
+    next_word: &str,
+) -> Result<(), std::io::Error> {
+    let result = add_sanitized_word(filename, word);
 
     match result {
         Ok(i) => {
-            let num = add_word(filename, next_word);
+            let num = add_sanitized_word(filename, next_word);
             if let Err(e) = num {
                 return Err(e);
             }
@@ -197,4 +283,35 @@ pub fn sneedov_append(filename: &str, word: &str, next_word: &str) -> Result<(),
     }
 }
 
-pub fn sneedov_generate() {}
+///Generates a new sentence from a markov chain file
+///
+///# Arguments
+///
+///* `filename` -  File name for the file to load from
+///
+pub fn sneedov_generate(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
+    //code goes here
+    let mut index = 1;
+    let mut sentence = String::new();
+
+    loop {
+        let line = get_line(filename, index);
+        let word = get_word(&line);
+
+        if word == END_KEYWORD {
+            break;
+        }
+        index = get_next_word(get_occurances(line.unwrap().as_str()).unwrap());
+
+        if word != START_KEYWORD {
+            let is_punc: bool = is_punctuation(word.parse::<char>());
+
+            if sentence.len() != 0 && !is_punc {
+                sentence.push_str(" ");
+            }
+            sentence.push_str(fix_fake_keyword(&word));
+        }
+    }
+
+    Ok(sentence)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,13 @@
 #![crate_name = "sneedov"]
 
-use sneedov::set_keywords;
-use sneedov::sneedov_append;
-
-fn split_punctuation(split: Vec<&str>) -> Vec<String> {
-    let punctuation = ['.', ',', '?', '!', ';', ':'];
-
-    let list = split.iter();
-    let mut vec = Vec::<String>::new();
-    for value in list {
-        let mut word = value.chars();
-        let last_char = word.next_back();
-        match last_char {
-            Some(..) => {
-                let mut clone = word.clone();
-                let second_last_char = clone.next_back();
-                match second_last_char {
-                    Some(..) => {
-                        if last_char.unwrap() != second_last_char.unwrap()
-                            && punctuation.contains(&last_char.unwrap())
-                        {
-                            vec.push(String::from(word.as_str()));
-                            vec.push(String::from(last_char.unwrap()));
-                        } else {
-                            vec.push(String::from(*value))
-                        }
-                    }
-                    None => vec.push(String::from(*value)),
-                }
-            }
-            None => (),
-        }
-    }
-    vec
-}
-
-macro_rules! split_sentence {
-    ($x:expr) => {{
-        let vec: Vec<&str> = $x.split_whitespace().collect();
-        let new_vec = split_punctuation(vec);
-        new_vec
-    }};
-}
-
-fn count_adjacent(vec: &Vec<String>) {
-    let mut iter = vec.iter().peekable();
-
-    if iter.peek().is_none() {
-        println!("Line is empty");
-    }
-
-    let mut is_first: bool = true;
-    while let Some(value) = iter.next() {
-        if is_first {
-            let _ = sneedov_append("test", "__start", value);
-            is_first = false;
-        }
-        //print!("{} {} ", previous, value);
-        if iter.peek().is_none() {
-            //println!("{}", "__end");
-            let _ = sneedov_append("test", value, "__end");
-        } else {
-            //println!("{}", iter.peek().unwrap());
-            let _ = sneedov_append("test", value, iter.peek().unwrap());
-        }
-    }
-}
+// use sneedov::sneedov_append_word;
+use sneedov::sneedov_generate;
 
 fn main() {
-    let sentence: &str = "🤣 LMAOOOOOO im dead";
+    // let sentence: &str = "🤣 LMAOOOOOO im dead";
 
-    let _ = set_keywords("test");
-    let words = split_sentence!(sentence);
-    count_adjacent(&words);
+    // let _ = set_keywords("test");
+    // let words = split_sentence!(sentence);
+    // count_adjacent(&words);
+    println!("{}", sneedov_generate("test").unwrap());
 }
